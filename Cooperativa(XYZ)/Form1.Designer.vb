cf3de36129ac939ccd1182e17c36fcd009a2dba6
Fernando Amador <amadorfernando1166@gmail.com>
2019-04-05 13:48:19 -0600

    Agregue Frm TipoPrestamo

--- Cooperativa(XYZ)/Form1.Designer.vb
+++ Cooperativa(XYZ)/Form1.Designer.vb
@@ -22,10 +22,183 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PCentarl = New System.Windows.Forms.Panel()
+        Me.PbxLogo = New System.Windows.Forms.PictureBox()
+        Me.LsvTipoPrestamo = New System.Windows.Forms.ListView()
+        Me.ChIdPrestamo = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ChPrestamo = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ChkVer = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BtnCerrar = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtIdPrestamo = New System.Windows.Forms.TextBox()
+        Me.TxtTipoPrestamo = New System.Windows.Forms.TextBox()
+        Me.PCentarl.SuspendLayout()
+        CType(Me.PbxLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PCentarl
+        '
+        Me.PCentarl.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PCentarl.Controls.Add(Me.PbxLogo)
+        Me.PCentarl.Controls.Add(Me.LsvTipoPrestamo)
+        Me.PCentarl.Controls.Add(Me.ChkVer)
+        Me.PCentarl.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PCentarl.Location = New System.Drawing.Point(0, 165)
+        Me.PCentarl.Name = "PCentarl"
+        Me.PCentarl.Size = New System.Drawing.Size(968, 303)
+        Me.PCentarl.TabIndex = 16
+        '
+        'PbxLogo
+        '
+        Me.PbxLogo.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PbxLogo.BackgroundImage = Global.Cooperativa_XYZ_.My.Resources.Resources.LogoCooperativa
+        Me.PbxLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PbxLogo.Location = New System.Drawing.Point(733, 248)
+        Me.PbxLogo.Name = "PbxLogo"
+        Me.PbxLogo.Size = New System.Drawing.Size(245, 71)
+        Me.PbxLogo.TabIndex = 13
+        Me.PbxLogo.TabStop = False
+        '
+        'LsvTipoPrestamo
+        '
+        Me.LsvTipoPrestamo.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.LsvTipoPrestamo.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ChIdPrestamo, Me.ChPrestamo})
+        Me.LsvTipoPrestamo.FullRowSelect = True
+        Me.LsvTipoPrestamo.GridLines = True
+        Me.LsvTipoPrestamo.Location = New System.Drawing.Point(393, 1)
+        Me.LsvTipoPrestamo.Name = "LsvTipoPrestamo"
+        Me.LsvTipoPrestamo.Size = New System.Drawing.Size(306, 302)
+        Me.LsvTipoPrestamo.TabIndex = 11
+        Me.LsvTipoPrestamo.UseCompatibleStateImageBehavior = False
+        Me.LsvTipoPrestamo.View = System.Windows.Forms.View.Details
+        '
+        'ChIdPrestamo
+        '
+        Me.ChIdPrestamo.Text = "Código Préstamo"
+        Me.ChIdPrestamo.Width = 100
+        '
+        'ChPrestamo
+        '
+        Me.ChPrestamo.Text = "Préstamo"
+        Me.ChPrestamo.Width = 200
+        '
+        'ChkVer
+        '
+        Me.ChkVer.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.ChkVer.AutoSize = True
+        Me.ChkVer.Location = New System.Drawing.Point(705, 6)
+        Me.ChkVer.Name = "ChkVer"
+        Me.ChkVer.Size = New System.Drawing.Size(70, 17)
+        Me.ChkVer.TabIndex = 12
+        Me.ChkVer.Text = "Ver Todo"
+        Me.ChkVer.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.GroupBox1.Controls.Add(Me.BtnCerrar)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TxtIdPrestamo)
+        Me.GroupBox1.Controls.Add(Me.TxtTipoPrestamo)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(968, 165)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        '
+        'BtnCerrar
+        '
+        Me.BtnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnCerrar.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnCerrar.BackgroundImage = Global.Cooperativa_XYZ_.My.Resources.Resources.IconCerrar
+        Me.BtnCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BtnCerrar.FlatAppearance.BorderSize = 0
+        Me.BtnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnCerrar.Location = New System.Drawing.Point(931, 132)
+        Me.BtnCerrar.Name = "BtnCerrar"
+        Me.BtnCerrar.Size = New System.Drawing.Size(37, 33)
+        Me.BtnCerrar.TabIndex = 13
+        Me.BtnCerrar.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(298, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(174, 22)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Código Préstamo:"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(323, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(149, 22)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Tipo Préstamo:"
+        '
+        'TxtIdPrestamo
+        '
+        Me.TxtIdPrestamo.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.TxtIdPrestamo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TxtIdPrestamo.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtIdPrestamo.Location = New System.Drawing.Point(478, 12)
+        Me.TxtIdPrestamo.Name = "TxtIdPrestamo"
+        Me.TxtIdPrestamo.ReadOnly = True
+        Me.TxtIdPrestamo.Size = New System.Drawing.Size(167, 29)
+        Me.TxtIdPrestamo.TabIndex = 10
+        '
+        'TxtTipoPrestamo
+        '
+        Me.TxtTipoPrestamo.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.TxtTipoPrestamo.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TxtTipoPrestamo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TxtTipoPrestamo.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtTipoPrestamo.Location = New System.Drawing.Point(478, 60)
+        Me.TxtTipoPrestamo.Name = "TxtTipoPrestamo"
+        Me.TxtTipoPrestamo.Size = New System.Drawing.Size(223, 29)
+        Me.TxtTipoPrestamo.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(968, 468)
+        Me.Controls.Add(Me.PCentarl)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.PCentarl.ResumeLayout(False)
+        Me.PCentarl.PerformLayout()
+        CType(Me.PbxLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PCentarl As Panel
+    Friend WithEvents PbxLogo As PictureBox
+    Friend WithEvents LsvTipoPrestamo As ListView
+    Friend WithEvents ChIdPrestamo As ColumnHeader
+    Friend WithEvents ChPrestamo As ColumnHeader
+    Friend WithEvents ChkVer As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents BtnCerrar As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtIdPrestamo As TextBox
+    Friend WithEvents TxtTipoPrestamo As TextBox
 End Class
